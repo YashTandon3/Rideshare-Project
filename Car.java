b222130b902b3091af89ad926e94f31ef19f4a50
Yash Tandon <ytandon27@students.polytechnic.org>
2025-02-24 09:26:29 -0800

    Changes to the car class

--- Car.java
+++ Car.java
@@ -1,5 +1,32 @@
 import java.util.*;
 public class Car {
+    //fields
+    private int destination;
+    private int currentLocation;
+    private boolean direction;
+    private ArrayList<Person> passengers;
 
+    //constructors
+    public Car(int myDestination, int start){
+        destination = myDestination;
+        currentLocation = start;
+        direction = destination > currentLocation;
+        passengers = new ArrayList<Person>();
+    }
     
+    public String toString(){
+        return super.toString() + " Destination " + destination + " Current location: " + currentLocation + " Going right? " + direction + " Passegers: " + passengers;
+    }
+
+    public void addPassenger(Person p){
+        if(passengers.size() >= 3){
+            System.out.println("Error no Room");
+        }else{
+            passengers.add(p);
+        }
+    }
+    public boolean hasRoom(){
+        return passengers.size()<3;
+    }
+
 }
