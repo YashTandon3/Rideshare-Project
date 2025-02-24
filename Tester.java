a31f65a1c26d8299523afb7de5bd97066e1ef6bb
Yash Tandon <ytandon27@students.polytechnic.org>
2025-02-24 09:29:01 -0800

    Tester edits

--- Tester.java
+++ Tester.java
@@ -6,5 +6,9 @@ public class Tester {
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
+        Car n = new Car(5, 9);
+        System.out.println(n);
+        n.addPassenger(a);
+        System.out.println(n);
     }
 }
