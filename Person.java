749b8050fdaa35576cda223bc76ce4dc375ee154
Yash Tandon <ytandon27@students.polytechnic.org>
2025-02-24 09:11:00 -0800

    Made the Person Class

--- Person.java
+++ Person.java
@@ -1,6 +1,35 @@
 import java.util.*;
 public class Person{
+  //fields
+  private boolean direction;
+  private int destination;
+  private int startLocation;
+   //true for going right false for left
 
+  //constructors
+  public Person(int myD, int myStart){
+      destination = myD;
+      startLocation = myStart;
+      direction = destination>startLocation;
+  }
+
+  //methods
+
+  public int getDestination(){
+      return destination;
+  }
+
+  public boolean getDirection(){
+      return direction;
+  }
+
+  public int getStart(){
+      return startLocation;
+  }
+
+  public String toString(){
+      return super.toString() + " start:  "+ startLocation + " Destinstion: " + destination + " Going Right?: " + direction;
+  }
 
 
     
---
+++ Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+    public static void main(String[] args) {
+        Person a = new Person(3, 7);
+        Person b = new Person(2, 0);
+        Person c = new Person(10, 24);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
