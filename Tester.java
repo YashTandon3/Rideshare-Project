7b927239030a92f586cf1916a31dc11128564348
Yash Tandon <ytandon27@students.polytechnic.org>
2025-03-13 09:00:59 -0700

    Made a scenario with forty cars instead

--- Tester.java
+++ Tester.java
@@ -4,47 +4,45 @@ public class Tester {
         r.populateCars(20);
         r.populateStations(50);
         System.out.println(r.toString());
-       
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
-        r.move();
+        for(int i = 0; i<32; i++){
+            r.move();
+        }
+
+     
  
         System.out.println(r.toString());
         System.out.println(r.numComplete());
 
-        System.out.println("Percent completed: ");
+        System.out.println("Percent completed with 20 cars: ");
         System.out.println((double)r.numComplete()/50*100);
 
 
 
+
+
+
+
+
+        Road s = new Road();
+        s.populateCars(40);
+        s.populateStations(50);
+        System.out.println(s.toString());
+        for(int i = 0; i<32; i++){
+            s.move();
+        }
+
+     
+ 
+        System.out.println(s.toString());
+        System.out.println("Number of people who have completed their trips in 40 cars: ");
+        System.out.println(s.numComplete());
+
+        System.out.println("Percent completed with 40 cars: ");
+        System.out.println((double)s.numComplete()/50*100);
+
+
+
     }
-}
+
+    }
+
