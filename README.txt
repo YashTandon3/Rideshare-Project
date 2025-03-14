06c334fd33a45d2a980daf67ec462b1c8ef274e3
Yash Tandon <ytandon27@students.polytechnic.org>
2025-03-13 21:07:57 -0700

    Added Readme and Finished the project

---
+++ README.txt
@@ -0,0 +1,8 @@
+In this project, we're running a car service to transport 50 passengers from point a to point B (Randomly generated)
+To do this, I coded multiple classes to run the service and survey how many passengers arrived at their location 
+within a span of 32 different rides. I found that the amount of people transported to their destination directly
+correlates to the amount of available cars. Logically, the more cars, the more eople arrive to their destination. 
+When expirementing, I found that 40-60% of people completed their trips when there were 20 cars available. On the 
+other hand, I found that 60-80% of passengers completed their trips when 40 cars were available. In conclusion, I 
+ended up finding that having more cars present directly leads to having more passengers complete their journies, 
+because each passenger has a slightly or majorly different stop.
--- Tester.java
+++ Tester.java
@@ -1,23 +1,26 @@
 public class Tester {
     public static void main(String[] args) {
         Road r = new Road();
+        System.out.println("Adding 20 Cars and 50 people...");
         r.populateCars(20);
         r.populateStations(50);
         System.out.println(r.toString());
+        System.out.println("Moving each car 32 times...");
         for(int i = 0; i<32; i++){
             r.move();
         }
 
      
- 
+        System.out.println("Displaying Car Data and Station Data");
         System.out.println(r.toString());
+        System.out.println("Number of Completed Passengers: ");
         System.out.println(r.numComplete());
 
         System.out.println("Percent completed with 20 cars: ");
         System.out.println((double)r.numComplete()/50*100);
 
 
-
+        System.out.println(" ");
 
 
 
@@ -26,13 +29,15 @@ public class Tester {
         Road s = new Road();
         s.populateCars(40);
         s.populateStations(50);
+        System.out.println("Changing Number of cars to 40...");
+        System.out.println("Moving each car 32 times...");
         System.out.println(s.toString());
         for(int i = 0; i<32; i++){
             s.move();
         }
 
      
- 
+        System.out.println("Displaying Car data and Station data: ");
         System.out.println(s.toString());
         System.out.println("Number of people who have completed their trips in 40 cars: ");
         System.out.println(s.numComplete());
